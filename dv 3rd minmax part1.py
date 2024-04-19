e0c3fc5a0e14bbe7a2fd167d26013783bb0be849
gowdahamsa <165622546+gowdahamsa@users.noreply.github.com>
2024-04-19 12:52:41 +0530

    Delete dv 3rd minmax part1.py

--- dv 3rd minmax part1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-data=pd.read_csv('minmax.csv', usecols=('ANNUAL_MIN', 'ANNUAL_MAX'))
-df=pd.DataFrame(data)
-df.plot(kind='hist',title='minimum and annual maximum temperature',color=('red','blue'))
-plt.xlabel('temperature')
-plt.ylabel('number of times')
-plt.show()
