23a76f75d5f29a02368971b2de2ac0ce52e68041
gowdahamsa <165622546+gowdahamsa@users.noreply.github.com>
2024-04-19 12:53:09 +0530

    Delete dv 4th res part2.py

--- dv 4th res part2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df=pd.read_csv("res12.csv")
-df.plot(kind='box',x='year',title='resturaunt',vert=False,patch_artist=True)
-plt.xlabel("resorts")
-plt.ylabel("rating")
-plt.show()
-
-
